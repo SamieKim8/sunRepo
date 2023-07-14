a8e6c636325cd94e17a02c96d6f25909e58ff534
Sunmi Kim <samiekim18@gmail.com>
2023-07-14 01:00:13 -0700

    Revert "Django Application"
    This reverts commit e46cf4e3042ea291078a02a386ea14536541f349.

--- my_Django_app/big_service/courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Course
-
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ("title", "credit", "grade", "description",)
-
-admin.site.register(Course, CourseAdmin)
